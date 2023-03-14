993335b7d1cd8c765ef69892e267c94fb0a946ac
unknown <nikhilkeshav76@gmail.com>
2023-03-14 19:14:51 +0530

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+  useTheme,
+  useMediaQuery,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import DrawerComponent from "./Drawer";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(5),
+    display: "flex",
+  },
+  logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    borderBottom: "1px solid transparent",
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  return (
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h5" className={classes.logo}>
+          MyBotTracker
+        </Typography>
+        {isMobile ? (
+          <DrawerComponent />
+        ) : (
+          <div className={classes.navlinks}>
+            <Button variant="contained" color="warning">
+              Join Discord
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default Navbar;
---
+++ src/pages/home.js
@@ -0,0 +1,6 @@
+import { Typography } from "@material-ui/core";
+
+function Home() {
+  return <Typography>This is the MyBots Tracker homepage</Typography>;
+}
+export default Home;
